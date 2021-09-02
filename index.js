1aff144a6dfd0074249d9a7b4599ca6a0c04715b
muhannadbm <mhd31145@hotmail.com>
2021-09-02 15:36:29 -0400

    mvp is done

---
+++ index.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+const crypto = require('crypto-js')
+const express = require('express')
+const cors = require('cors')
+const users = require('./users/users')
+
+const server = express()
+server.use(express.json())
+server.use(cors())
+
+server.get('/api/user', (req,res)=>{
+    res.json({data: users})
+})
+
+server.post('/api/register', (req,res)=>{
+    if(!req.body.username || !req.body.password){
+        res.status(400).json({message: "missing name or email"})
+    }
+    const Encrypted_password = crypto.AES.encrypt(req.body.password, process.env.SECRET_WORD).toString()
+    req.body.id = Date.now()
+    req.body.password = Encrypted_password
+    let new_users = [...users, req.body]
+    res.status(201).json({new_users})
+})
+
+server.post('/api/login', (req,res)=>{
+    if(!req.body.username || !req.body.password){
+        res.status(401).json({message: "missing some credentials !"})
+    }
+
+
+    users.forEach(user => {
+        const bytes = crypto.AES.decrypt(user.password, process.env.SECRET_WORD)
+        const Decrypted_password = bytes.toString(crypto.enc.Utf8)
+        if(user.username === req.body.username && Decrypted_password === req.body.password){
+            res.status(200).json({message: `Welcome ${req.body.username}` })
+        }
+    });
+    res.status(404).json({message: "user or password is not correct !"})
+    
+})
+server.listen(process.env.PORT, ()=>{
+    console.log('listening on Port ', process.env.PORT)
+})
---
+++ users/users.js
@@ -0,0 +1,19 @@
+const users = [{id: 1, username: 'Moe', password: 'U2FsdGVkX184PILxiV96s3F/1MQMdXEZY3reJbNZjFY='},
+{id: 2, username: 'John', password: 'U2FsdGVkX19MdvyLuCsSU3YjWoBvzZ6xr4ASPmA5GdY='},
+{id: 3, username: 'Robert', password: 'U2FsdGVkX19iqp2qibAaajiMs9NohHN3hlZWd94cu3A='},
+{id: 4, username: 'Vicky', password: 'U2FsdGVkX18CAth7Pl3mHL3YiwuYSSCVusO8SwJYiLI='},
+{
+    "username": "Mary",
+    "password": "U2FsdGVkX1/DQtaiAH7+cWZVYr/GEL2hI8Q3g6i0HVE=",
+    "id": 1630611220054
+}]
+
+module.exports = users
+
+
+//{id: 3, username: 'Robert', password: 'zxcv'},
+//{id: 4, username: 'Vicky', password: 'qwert'}]
+//{id: 3, username: 'Moe', password: '123456'},
+//{id: 4, username: 'John', password: 'asdf'}]
+//{"username": "Mary", "password": "supersecretword",
+
